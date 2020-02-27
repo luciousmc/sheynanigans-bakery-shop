a0e0a7ce68bdb6490bcab21ced2cd19b814443bb
Marlon <luciousmc@icloud.com>
2020-02-27 12:30:48 -0800

    endpoint for getting all products completed

--- server/index.js
+++ server/index.js
@@ -19,6 +19,16 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/products', (req, res, next) => {
+  const sql = `
+    SELECT  "productId", "name", "price",
+            "image", "shortDescription"
+    FROM    "products"`;
+  db.query(sql)
+    .then(response => res.json(response.rows))
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
