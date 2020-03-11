ccdc6a0ac1420bb4ffcf61baf14cbd5a90e6334f
Marlon Clay <luciousmc@icloud.com>
2020-03-10 23:49:07 -0700

    adds padding on body reduce width of product list items by 1rem

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductListItem(props) {
   return (
     <div className="col">
-      <div className="card mt-3 card-item" style={ { width: '21rem' } }>
+      <div className="card mt-3 card-item" style={ { width: '20rem' } }>
         <img className="card-img-top" src={ props.product.image } alt="Card image cap" />
         <div className="card-body">
           <h5 className="card-title">{ props.product.name }</h5>
