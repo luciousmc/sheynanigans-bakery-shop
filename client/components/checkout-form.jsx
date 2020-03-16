eb95cb265433e4371b6b4856496b04c034fe3db0
Marlon Clay <luciousmc@icloud.com>
2020-03-15 22:06:30 -0700

    adds error text for all fields

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -7,8 +7,14 @@ class CheckoutForm extends React.Component {
     this.state = {
       name: '',
       nameError: '',
-      shippingAddress: '',
+      addressLine: '',
       addressError: '',
+      city: '',
+      cityError: '',
+      state: '',
+      stateError: '',
+      zipcode: '',
+      zipcodeError: '',
       creditCard: '',
       creditCardError: ''
     };
@@ -31,18 +37,63 @@ class CheckoutForm extends React.Component {
           this.setState({ nameError: 'Please enter at least 5 characters' });
         }
         break;
-      case 'shippingAddress':
+      case 'addressLine':
+        if (!value) {
+          this.setState({ addressError: 'Address is required' });
+        } else if (value.length < 6) {
+          this.setState({ addressError: 'Please enter at least 6 characters for address' });
+        }
+        break;
+      case 'city':
+        if (!value) {
+          this.setState({ cityError: 'City is required' });
+        } else if (value.length < 3) {
+          this.setState({ cityError: 'Please enter at least 3 characters for city' });
+        }
+        break;
+      case 'state':
+        if (!value) {
+          this.setState({ stateError: 'State is required' });
+        } else if (!isNaN(value)) {
+          this.setState({ stateError: 'Please use 2 letter state code' });
+        }
+        break;
+      case 'zipcode':
+        if (!value) {
+          this.setState({ zipcodeError: 'Zip is required' });
+        } else if (isNaN(value)) {
+          this.setState({ zipcodeError: 'Please use 2 letter state code' });
+        }
+        break;
       case 'creditCard':
+        if (!value) {
+          this.setState({ creditCardError: 'Credit Card is required' });
+        }
     }
     console.log('this just got blurred witH: ', e.target);
   }
 
-  clearErrors() {
-    this.setState({
-      nameError: '',
-      addressError: '',
-      creditCardError: ''
-    });
+  clearErrors(field) {
+    switch (field) {
+      case 'name':
+        this.setState({ nameError: '' });
+        break;
+      case 'addressLine':
+        this.setState({ addressError: '' });
+        break;
+      case 'city':
+        this.setState({ cityError: '' });
+        break;
+      case 'state':
+        this.setState({ stateError: '' });
+        break;
+      case 'zipcode':
+        this.setState({ zipcodeError: '' });
+        break;
+      case 'creditCard':
+        this.setState({ creditCardError: '' });
+        break;
+    }
   }
 
   handleChange(e) {
@@ -53,8 +104,8 @@ class CheckoutForm extends React.Component {
 
   render() {
     const { total } = this.props;
-    const { name, shippingAddress, creditCard } = this.state;
-    const { nameError, addressError, creditCardError } = this.state;
+    const { name, addressLine, city, state, zipcode, creditCard } = this.state;
+    const { nameError, addressError, cityError, stateError, zipcodeError, creditCardError } = this.state;
 
     return (
       <section className="container w-50 checkout-container">
@@ -85,9 +136,9 @@ class CheckoutForm extends React.Component {
                   className="form-control"
                   id="name"
                   onChange={ this.handleChange }
-                  value={ name }
                   onBlur={ this.validateFields }
-                  onFocus={ this.clearErrors }
+                  onFocus={ () => this.clearErrors('name') }
+                  value={ name }
                   maxLength="65"
                   required
                 />
@@ -95,23 +146,76 @@ class CheckoutForm extends React.Component {
                   { nameError && nameError }
                 </div>
               </div>
-              <div className="form-group">
+
+              <div className="form-group mb-3">
                 <label htmlFor="address">Address:</label>
                 <input
-                  name="shippingAddress"
-                  type="textarea"
+                  name="addressLine"
+                  type="text"
                   className="form-control"
                   id="address"
                   onChange={ this.handleChange }
                   onBlur={ this.validateFields }
-                  onFocus={ this.clearErrors }
-                  value={ shippingAddress }
+                  onFocus={ () => this.clearErrors('addressLine') }
+                  value={ addressLine }
                   required
                 />
                 <div className="address-error">
                   { addressError && addressError }
                 </div>
+                <div className="address-details d-flex justify-content-between mt-3">
+                  <div className="city">
+                    <label htmlFor="city">City:</label>
+                    <input
+                      type="text"
+                      name="city"
+                      className="form-control"
+                      onChange={ this.handleChange }
+                      onBlur={ this.validateFields }
+                      onFocus={ () => this.clearErrors('city') }
+                      value={ city }
+                      required
+                    />
+                    <div className="city-error">
+                      { cityError && cityError }
+                    </div>
+                  </div>
+                  <div className="state">
+                    <label htmlFor="city">State:</label>
+                    <input
+                      type="text"
+                      name="state"
+                      className="form-control"
+                      onChange={ this.handleChange }
+                      onBlur={ this.validateFields }
+                      onFocus={ () => this.clearErrors('state') }
+                      value={ state }
+                      maxLength="2"
+                      required
+                    />
+                    <div className="state-error">
+                      { stateError && stateError }
+                    </div>
+                  </div>
+                  <div className="zipcode">
+                    <label htmlFor="city">Zip:</label>
+                    <input
+                      type="text"
+                      name="zipcode"
+                      className="form-control"
+                      onChange={ this.handleChange }
+                      onBlur={ this.validateFields }
+                      onFocus={ () => this.clearErrors('zipcode') }
+                      value={ zipcode }
+                      required
+                    />
+                    <div className="zipcode-error">
+                      { zipcodeError && zipcodeError }
+                    </div>
+                  </div>
+                </div>
               </div>
+
               <div className="form-group">
                 <label htmlFor="credit-card">Credit Card Number:</label>
                 <input
@@ -121,7 +225,7 @@ class CheckoutForm extends React.Component {
                   id="credit-card"
                   onChange={ this.handleChange }
                   onBlur={ this.validateFields }
-                  onFocus={ this.clearErrors }
+                  onFocus={ () => this.clearErrors('creditCard') }
                   value={ creditCard }
                   required
                 />
