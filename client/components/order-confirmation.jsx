e29c0cdf75beee99ed37b1e92de7bf7e57304f50
Marlon <luciousmc@icloud.com>
2020-04-01 14:15:52 -0700

    adapts order confirmation to smaller screens

--- client/components/order-confirmation.jsx
+++ client/components/order-confirmation.jsx
@@ -6,7 +6,7 @@ const OrderConfirmation = ({ setView, customer, orderItems, total }) => {
   const [itemCount] = useGetItemCount(orderItems);
 
   return (
-    <section className="container w-50 rounded order-confirm-container">
+    <section className="container rounded order-confirm-container">
       <div className="row confirm-header my-3">
         <div className="col-12">
           <h1 className="text-center">Thank you for your order!</h1>
@@ -15,84 +15,88 @@ const OrderConfirmation = ({ setView, customer, orderItems, total }) => {
 
       <div className="row confirm-subtext">
         <div className="col-12">
-          <p className="text-center">This is a demo site so no order will be processed </p>
+          <p className="text-center text-danger">This is a demo site so no order will be processed </p>
         </div>
       </div>
 
-      <div className="row order-number mt-5">
+      <div className="row order-number mt-3 mt-sm-5">
         <div className="col">
           <h2>Order: #{ customer.orderId }</h2>
         </div>
       </div>
 
-      <div className="row product-details mt-5">
+      <div className="row product-details mt-3 mt-sm-5">
         <div className="col">
           <h3>Order Details</h3>
         </div>
       </div>
       <div className="row order-details">
         <div className="col">
-          <table className="order-table table p-3">
-            <thead>
-              <tr>
-                <th scope="col">Product</th>
-                <th scope="col">Quantity</th>
-                <th scope="col">Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                cartInfo.map(item => {
-                  const { multiplier } = itemCount[item.productId];
-                  return (
-                    <tr key={ item.cartItemId }>
-                      <td>{ item.name }</td>
-                      <td className="font-italic">x{ multiplier }</td>
-                      <td className="font-italic">${ (multiplier * item.price / 100).toFixed(2) }</td>
-                    </tr>
-                  );
+          <div className="table-responsive-sm">
+            <table className="order-table table p-3">
+              <thead>
+                <tr>
+                  <th scope="col">Product</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  cartInfo.map(item => {
+                    const { multiplier } = itemCount[item.productId];
+                    return (
+                      <tr key={ item.cartItemId }>
+                        <td>{ item.name }</td>
+                        <td className="font-italic">x{ multiplier }</td>
+                        <td className="font-italic">${ (multiplier * item.price / 100).toFixed(2) }</td>
+                      </tr>
+                    );
+                  }
+                  )
                 }
-                )
-              }
-              <tr>
-                <td></td>
-                <th>Purchase Total:</th>
-                <td>${ (total / 100).toFixed(2) }</td>
-              </tr>
+                <tr>
+                  <td></td>
+                  <th>Purchase Total:</th>
+                  <td>${ (total / 100).toFixed(2) }</td>
+                </tr>
 
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
       <div className="row customer-details mt-5">
         <div className="col">
-          <h3>Customer Details</h3>
+          <h3 className="text-center text-sm-left">Customer Details</h3>
         </div>
       </div>
       <div className="row customer-info">
         <div className="col">
-          <table className="table-sm">
-            <tbody>
-              <tr>
-                <th scope="row">Name:</th>
-                <td>{ customer.name }</td>
-              </tr>
-              <tr>
-                <th scope="row">Address:</th>
-                <td>{ customer.shippingAddress }</td>
-              </tr>
-              <tr>
-                <th scope="row">Credit Card:</th>
-                <td>Number ending with { customer.creditCard.slice(12) }</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="table-responsive">
+            <table className="table">
+              <tbody>
+                <tr>
+                  <th scope="row">Name:</th>
+                  <td>{ customer.name }</td>
+                </tr>
+                <tr>
+                  <th scope="row">Address:</th>
+                  <td>{ customer.shippingAddress }</td>
+                </tr>
+                <tr>
+                  <th scope="row">Credit Card:</th>
+                  <td>Number ending with { customer.creditCard.slice(12) }</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
       <div className="row">
-        <div className="col text-right p-3">
+        <div className="col text-center text-sm-right p-3">
           <button className="btn btn-dark" onClick={ () => setView('catalog', {})}>Done</button>
         </div>
       </div>
