a7cba4148d628329879becc918bac50b68562a1f
Marlon <luciousmc@icloud.com>
2020-04-04 23:56:52 -0700

    changes quantity to input type 'number'

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -58,14 +58,15 @@ function CartSummaryItem({ cartItem, addToCart, setView, showConfirmDeleteModal,
           <div className="col">
             <div className="form-group text-right">
               <label htmlFor="qty">Qty:</label>
-              <select name="quantity" id="qty" value={ quantity } onChange={ onQtyChange }>
+              {/* <select name="quantity" id="qty" value={ quantity } onChange={ onQtyChange }>
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
-              </select>
+              </select> */}
+              <input type="number" name="quantity" id="qty" value={ quantity } onChange={ onQtyChange } />
             </div>
           </div>
         </div>
